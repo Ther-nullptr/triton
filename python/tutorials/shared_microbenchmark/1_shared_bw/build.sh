39f24081793b656159799e22388cb94589b2c9a0
Yujin Wang <1329438302@qq.com>
2023-07-17 06:31:44 +0000

    chore: add shared memory benchmark

---
+++ python/tutorials/shared_microbenchmark/1_shared_bw/build.sh
@@ -0,0 +1,1 @@
+nvcc --resource-usage -Xptxas -dlcm=cv -Xptxas -dscm=wt       -gencode=arch=compute_80,code=\"sm_80,compute_80\"  shared_bw.cu -o shared_bw -I/home/zli/workspace/cuda-samples/Common -L -lcudart
---
+++ python/tutorials/shared_microbenchmark/2_shared_lat/build.sh
@@ -0,0 +1,1 @@
+nvcc --resource-usage -Xptxas -dlcm=cv -Xptxas -dscm=wt       -gencode=arch=compute_80,code=\"sm_80,compute_80\"  shared_lat.cu -o shared_lat -I/home/zli/workspace/cuda-samples/Common -L -lcudart
